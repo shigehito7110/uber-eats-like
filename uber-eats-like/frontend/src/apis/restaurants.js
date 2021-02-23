f15e0a21630bcdbe1301cfcf5e74395d0d8a1fad
shigehito7110 <shige.7110.330@gmail.com>
2021-02-23 17:21:18 +0900

    [Update] 一時避難

--- uber-eats-like/frontend/src/apis/restaurants.js
+++ uber-eats-like/frontend/src/apis/restaurants.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { restaurantsindex } from '../urls/index.js'
 
-export const fechRestaurants = () => {
+export const fetchRestaurants = () => {
   return axios.get(restaurantsindex)
     .then(res => {
       return res.data
