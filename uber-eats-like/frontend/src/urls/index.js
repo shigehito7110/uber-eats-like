5a0c921645d84c66124953d54253b84a5b918d44
shigehito7110 <shige.7110.330@gmail.com>
2021-02-24 20:56:46 +0900

    [Update] 食べ物一覧のレンダリング

--- uber-eats-like/frontend/src/urls/index.js
+++ uber-eats-like/frontend/src/urls/index.js
@@ -1,7 +1,7 @@
 const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
 
 export const restaurantsindex = `${DEFAULT_API_LOCALHOST}/restaurants`;
-export const foodsindex = (restaurantId) =>
+export const foodsIndex = (restaurantId) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
 export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods`;
 export const lineFoodsReplce = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
